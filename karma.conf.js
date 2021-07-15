e60a7a122b56aa6f4cdbe6a75876a7dc9dc644f5
Patrick Uhlmann <git@uhlme.ch>
2021-07-15 21:08:40 +0200

    configure that karma fails on console warnings/errors

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,11 @@
+const { join } = require('path');
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
     basePath: "",
+    files: [join(__dirname, 'karma.globals.js')],
     frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
       require("karma-jasmine"),
---
+++ karma.globals.js
@@ -0,0 +1,3 @@
+// let the tests fail if a warning or error is logged
+console.warn = (message) => { throw new Error(message); };
+console.error = (message) => { throw new Error(message); };

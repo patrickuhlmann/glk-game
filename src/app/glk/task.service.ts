52330f0c6679e13a2b092fd0d7b9103140e5d316
Patrick Uhlmann <git@uhlme.ch>
2021-08-01 23:52:13 +0200

    add missing tasks

--- src/app/glk/task.service.ts
+++ src/app/glk/task.service.ts
@@ -170,10 +170,216 @@ export class TaskService {
     questions: [],
   };
   static ESG_UNIFY_QUIZ: Quiz = {
-    questions: [],
+    questions: [
+      {
+        question: 'Welche Aussagen über den ESG Day sind korrekt?',
+        answers: [
+          {
+            answer: 'Der ESG Day findet jährlich statt.',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer: 'Der ESG Day findet alle zwei Jahre statt.',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: 'Der ESG Day wird jeweils vom ESG Commitee organisiert.',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer:
+              'Der ESG Day wird jeweils von einem gewählten Land (z. B. Deutschland, Frankreich, ...) organisiert.',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer:
+              'Ein wichtiger Bestandteil des ESG Day ist das Versenden von Postkarten an das organisierende Land.',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer:
+              'Ein wichtiger Bestandteil des ESG Day ist ein Telefonat mit einer Gruppe aus einem anderen Land.',
+            checked: false,
+            isCorrect: false,
+          },
+        ],
+        hint: '',
+      },
+      {
+        question:
+          'Welche Aussagen über das ESG Leadership Training and Seminar sind korrekt?',
+        answers: [
+          {
+            answer: 'Das Treffen dauert jeweils eine Woche.',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: 'Das Treffen dauert jeweils vier Tage.',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer:
+              'Es gibt einen Teil für Minderjährige und einen Teil für Erwachsene.',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer:
+              'Sowohl Minderjährige wie auch Erwachsene folgen demselben Programm',
+            checked: false,
+            isCorrect: true,
+          },
+        ],
+        hint: '',
+      },
+      {
+        question: 'Welche Aussagen über das ESG Jamboree sind korrekt?',
+        answers: [
+          {
+            answer:
+              'Es handelt sich um ein Lager an welchem alle Jungscharen von Europa teilnehmen können.',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer:
+              'Das Jamboree 2021 findet unter Berücksichtigung von Corona Massnahmen statt.',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer:
+              'Anstelle des physischen Jamboree 2021 wurde ein Programmpackage zusammengestellt, welches diverse Aktivitäten beinhaltet',
+            checked: false,
+            isCorrect: true,
+          },
+        ],
+        hint: '',
+      },
+      {
+        question: 'Auf welchen Ebenen finden Unify Konferenzen statt?',
+        answers: [
+          {
+            answer: 'National',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer: 'Europäisch',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer: 'Weltweit',
+            checked: false,
+            isCorrect: true,
+          },
+        ],
+        hint: '',
+      },
+    ],
   };
   static YMCA_QUIZ: Quiz = {
-    questions: [],
+    questions: [
+      {
+        question: 'Welche Aussagen über den YMCA World sind korrekt?',
+        answers: [
+          {
+            answer: 'Das Hauptquartier befindet sich in Genf',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer:
+              'Es werden 5 der 17 United Nations Sustainable Development Goals (SDGs) abgedeckt',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer:
+              'Die vier Hauptbereiche sind: Gesundheit, Arbeit, ziviles Engangement und Umwelt.',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer:
+              'Der YMCA setzt primär auf erfahrene Erwachsene, welche die Jungen motivieren und anleiten sollen.',
+            checked: false,
+            isCorrect: true,
+          },
+        ],
+        hint: '',
+      },
+      {
+        question: 'Welche Länder gehören zum YMCA Europe?',
+        answers: [
+          {
+            answer: 'Deutschland',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer: 'Frankreich',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer: 'Lichtenstein',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: 'Belgien',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: 'Finnland',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer: 'Schweden',
+            checked: false,
+            isCorrect: true,
+          },
+        ],
+        hint: '',
+      },
+      {
+        question: 'Welche Programmgruppen führt der YMCA Europe?',
+        answers: [
+          {
+            answer: 'YMCA Civic Engagement Group',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: 'YMCA Europe Working Group for Environment',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: 'YMCA Europe Working Group on Refugees',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer: 'Volunteers for Europe',
+            checked: false,
+            isCorrect: true,
+          },
+        ],
+        hint: '',
+      },
+    ],
   };
   static YWCA_QUIZ: Quiz = {
     questions: [],
@@ -185,10 +391,213 @@ export class TaskService {
     questions: [],
   };
   static REGIONEN_QUIZ: Quiz = {
-    questions: [],
+    questions: [
+      {
+        question:
+          'Wann findet der Taku Musik der Cevi Region Bern dieses Jahr statt?',
+        answers: [
+          {
+            answer: '12.08.2021 08:00 - 15.08.2021',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: '10.-17.10.2021',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: '14.-17.10.2021',
+            checked: false,
+            isCorrect: true,
+          },
+        ],
+        hint: 'Siehe: Webseite der Region Bern unter Cevianerin/Kursangebot/Cevi Kurse',
+      },
+      {
+        question: 'Welchen Kurs bietet Outdoortech nicht an?',
+        answers: [
+          {
+            answer: 'Sarasani- und Seiltechnikkurs',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: 'Frauen Pioniertechnikkurs',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: 'Männer Pioniertechnikkurs',
+            checked: false,
+            isCorrect: true,
+          },
+        ],
+        hint: 'Siehe: Webseite von Outdoortech (Region Bern) unter Lager/ Kurse',
+      },
+      {
+        question:
+          'Was ist die grosse Leidenschaft des Leitungsteams des Hattricks?',
+        answers: [
+          {
+            answer: 'Basketball und Kinder',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: 'Fussball und Jesus',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer: 'Posaunen und Singen',
+            checked: false,
+            isCorrect: false,
+          },
+        ],
+        hint: 'Siehe: Webseite des Cevi Basel unter Angebote',
+      },
+      {
+        question:
+          'Ab welchem Alter, sollten Jungscharkinder nach dem Tatkundefilter das Alarmieren können?',
+        answers: [
+          {
+            answer: '10 Jahre',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer: '12 Jahre',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: '13 Jahre',
+            checked: false,
+            isCorrect: false,
+          },
+        ],
+        hint: 'Siehe: Webseite der Region Winterthur-Schaffhausen unter Cevi-Online/Tatkunde',
+      },
+      {
+        question: 'Wo ist das Ladenlokal des Cevi Zürich?',
+        answers: [
+          {
+            answer: 'Sihlstrasse 33, 8001 Zürich',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer: 'Bahnhofstrasse 24, 8001 Zürich',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: 'Stauffacherstrasse 16, 8001 Zürich',
+            checked: false,
+            isCorrect: false,
+          },
+        ],
+        hint: 'Siehe: Webseite der Cevi Zürich (Stadt) unter Angebote/Lokal/Ort/Standort',
+      },
+      {
+        question: 'Welches Rezept gibt es im Cevi Wiki?',
+        answers: [
+          {
+            answer: 'Älplermagronen hindersi',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: 'Chüssischlacht auf dem Feuer',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer: 'Urchige Marshmallow-Sandwichs',
+            checked: false,
+            isCorrect: false,
+          },
+        ],
+        hint: 'Siehe: Webseite Cevi Wiki (Region Zürich) unter Rezepte',
+      },
+    ],
   };
   static CEVI_SCHWEIZ_QUIZ: Quiz = {
-    questions: [],
+    questions: [
+      {
+        question:
+          'Welcher der folgenden Slogans steht in der Strategie 2030 des Cevi Schweiz?',
+        answers: [
+          {
+            answer: 'Der Cevi ist wirtschaftlich relevant',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer:
+              'Der Cevi ist eine Gemeinschaft von Kindern und Jugendlichen',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: 'Der Cevi steht auf christlichem Fundament',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer:
+              'Der Cevi baut auf starke Strukturen und schlanke Beziehungen',
+            checked: false,
+            isCorrect: false,
+          },
+        ],
+        hint: 'Unter Portrait findet ihr das Leitbild, sowie die Strategie 2030 des Cevi Schweiz. Lest das Leitbild und schaut euch die 4 Hauptslogans der Strategie 2030 an.',
+      },
+      {
+        question: 'Wann findet des Cevi-Tag 2021 statt?',
+        answers: [
+          {
+            answer: '11. September 2021',
+            checked: false,
+            isCorrect: true,
+          },
+          {
+            answer: '18. September 2021',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: '25. September 2021',
+            checked: false,
+            isCorrect: false,
+          },
+        ],
+        hint: 'Events und Kurse: Die nationalen Events/Kurse des Cevi Schweiz, sowie deren Jahresplan findest du auf der Webseite unter Events und Ausbildung.',
+      },
+      {
+        question:
+          'Welche Themenbereiche werden unter Hilfsmittel/Themen/Merkblätter aufgegriffen?',
+        answers: [
+          {
+            answer: 'LGBTQ, Interkulturelle Öffnung, Finanzen',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: 'Fröschli, Handbuch Abteilungsleitende, Prävention',
+            checked: false,
+            isCorrect: false,
+          },
+          {
+            answer: 'Versicherungen, Krisenmanagement, Vereinsgründung',
+            checked: false,
+            isCorrect: true,
+          },
+        ],
+        hint: 'Hilfsmittel/ Themen/ Merkblätter: Verschaffe dir einen groben Überblick zu welchen Themen es hier Unterlagen gibt.',
+      },
+    ],
   };
 
   public static HORYZON_TASK = {
